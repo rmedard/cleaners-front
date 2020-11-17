109369275d61f083e509947cb4ddcc1f8fea7b76
Medard Rebero <medard.rebero@gmail.com>
2020-11-17 10:35:57 +0100

    Refactor

--- src/app/pages/become-member-page/become-member-page.component.ts
+++ src/app/pages/become-member-page/become-member-page.component.ts
@@ -1,7 +1,9 @@
 import {Component, OnInit} from '@angular/core';
-import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
-import {faPlus, faUser, faUserTie} from '@fortawesome/free-solid-svg-icons';
+import {FormArray, FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
+import {faEuroSign, faMinus, faPlus, faUser, faUserTie} from '@fortawesome/free-solid-svg-icons';
 import {UserType} from '../../+models/user';
+import {Service} from '../../+models/service';
+import {ServicesService} from '../../+services/services.service';
 
 @Component({
   selector: 'app-become-member-page',
@@ -13,12 +15,27 @@ export class BecomeMemberPageComponent implements OnInit {
   userIcon = faUser;
   professionalIcon = faUserTie;
   addIcon = faPlus;
+  removeIcon = faMinus;
+  euroIcon = faEuroSign;
+  services: Service[] = [];
+  proposedServices: FormArray = {} as FormArray;
+  userType = UserType.CUSTOMER.valueOf();
 
-  constructor(private formBuilder: FormBuilder) { }
+  constructor(private formBuilder: FormBuilder, private servicesService: ServicesService) {
+  }
 
   ngOnInit(): void {
+    this.servicesService.getServices().subscribe(data => {
+      this.services = data;
+      this.proposedServices = new FormArray([
+        new FormGroup({
+          regService: new FormControl(this.services[0]),
+          regHourlyRate: new FormControl(0.00, [Validators.min(0)])
+        })
+      ]);
+    });
     this.registrationForm = this.formBuilder.group({
-      userTypeField: [UserType.CUSTOMER.valueOf(), Validators.required],
+      userTypeField: [this.userType, Validators.required],
       regLastNameField: new FormControl('', [Validators.required]),
       regFirstNameField: new FormControl('', [Validators.required]),
       regEmailField: new FormControl('', [Validators.email]),
@@ -26,8 +43,37 @@ export class BecomeMemberPageComponent implements OnInit {
       regStreetField: new FormControl('', [Validators.required]),
       regPlotNumberField: new FormControl('', [Validators.required]),
       regCityField: new FormControl('', [Validators.required]),
-      regPostalCodeField: new FormControl('', [Validators.required])
+      regPostalCodeField: new FormControl('', [Validators.required]),
+      regProposedServices: this.proposedServices
     });
   }
 
+  toggleUserType(userType: string): void {
+    if (this.userType !== userType) {
+      this.userType = userType;
+    }
+  }
+
+  removeProposedService(index: number): void {
+    this.proposedServices.removeAt(index);
+  }
+
+  addProposedService(): void {
+    this.proposedServices.insert(this.proposedServices.length, new FormGroup({
+      regService: new FormControl(this.services.filter(s => !this.isSelected(s))[0]),
+      regHourlyRate: new FormControl(0.00, [Validators.min(0)])
+    }));
+  }
+
+  isSelected(service: Service): boolean {
+    const selectedServices = this.proposedServices.controls.map(c => {
+      const formGroup = c as FormGroup;
+      return formGroup.controls.regService.value as Service;
+    }) as Service[];
+    return selectedServices.includes(service);
+  }
+
+  canAddServices(): boolean {
+    return this.proposedServices.length === this.services.length;
+  }
 }
